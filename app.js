6f2241f6e4b11b7b937f6da0f86746272ed81bce
danagilliann <danalee96@gmail.com>
2015-05-25 01:53:35 -0400

    ugh much tired idk what im doing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ angular.module('flapperNews', ['ui.router'])
 app.controller('MainCtrl', [
 '$scope',
 'posts',
-function($scope, posts) {
+function($scope, post) {
 	$scope.test = 'Hello world!';
 	$scope.posts = posts.posts;
 	$scope.addPost = function() {
@@ -14,12 +14,16 @@ function($scope, posts) {
 			title: $scope.title,
 			link: $scope.link, 
 			upvotes: 0
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
 		});
 		$scope.title = '';
 		$scope.link = '';
 	};
-	$scope.incrementUpvotes = function(postees) {
-		postees.upvotes += 1;
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
 	};
 }]);
 
@@ -41,10 +45,20 @@ function($stateProvider, $urlRouterProvider) {
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
 		});
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
 
 	$urlRouterProvider.otherwise('home');
 }]);
 
-
-
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+}]);
 
